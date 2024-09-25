ed02a485c533a43bb689c718df9d9b89d785f4e9
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-26 00:53:11 +0530

    Scrape Exact

--- webscraping/ScrapeExactData/ww.py
+++ webscraping/ScrapeExactData/ww.py
@@ -4,17 +4,21 @@ from bs4 import BeautifulSoup
 
 def extract_data_from_url(url, file_name):
     response = requests.get(url)
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        page_content = soup.get_text()
+
+        elements = soup.find_all('div', class_='quote')
+
         with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(page_content)
+            for element in elements:
+                file.write(element.get_text().strip() + '\n')
+
         print(f"Data successfully written to {file_name}")
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
 
-
-url = 'https://patpat.lk/vehicle'
-file_name = 'extracted_data.txt'
+url = 'https://quotes.toscrape.com/'
+file_name = 'ww.txt'
 extract_data_from_url(url, file_name)
